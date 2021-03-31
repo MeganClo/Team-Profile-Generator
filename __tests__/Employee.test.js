6aacd6cc354aba92d7a835296734841f5514a9d9
Megs <Megs.close@gmail.com>
2021-03-30 17:37:41 -0700

    setting up some first tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,11 @@
+const { TestScheduler } = require('jest');
 const Employee = require('../lib/Employee');
+
+test('creates a new employee object', () => {
+    const employee = new Employee();
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
